a94394abafca3f74ca31c19d0626ebe7d2a8adbc
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2024-01-04 12:34:08 +0200

    update

--- Mapty/script.js
+++ Mapty/script.js
@@ -10,3 +10,39 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//2) Using the Geoloaction API ----------------------------------------
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // const latitude = position.coords.latitude;
+      // const longitude = position.coords.longitude;
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //{ latitude } and { longitude } are examples of destructuring assignment, where it allows you to create variables named latitude and longitude and assign them the values of the corresponding properties from position.coords.
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13); //after coords, 13-is zoom level
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert(`Could not get your current position`);
+    }
+  );
+}
+
+//working because other.js is above than script.js and its like global
+//Thats why Leaflet is global and we can use it!
+// console.log(firstName);
