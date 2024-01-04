76f3748053c7f81dbc9af51a2bba04c5ce6c6285
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2024-01-04 13:53:02 +0200

    update

--- Mapty/script.js
+++ Mapty/script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
 //2) Using the Geoloaction API ----------------------------------------
 
 if (navigator.geolocation) {
@@ -26,36 +27,20 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       //3) Displaying a Map Using Leaflet Library
-      const map = L.map('map').setView(coords, 13); //after coords, 13-is zoom level
+      map = L.map('map').setView(coords, 13); //after coords, 13-is zoom level
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      // L.marker(coords)
-      //   .addTo(map)
-      //   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      //   .openPopup();
-
       //3) display a Map Marker
-      //we Gonna use on() instead of addEventListener() it comes from Leaflet library
-      map.on('click', function (mapEvent) {
-        // console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent(`Workout`)
-          .openPopup();
+      //Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        //4) Rendering Workout Input Form
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -64,6 +49,37 @@ if (navigator.geolocation) {
   );
 }
 
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  //Claer input fiels
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  //Display Marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent(`Workout`)
+    .openPopup();
+});
 //working because other.js is above than script.js and its like global
 //Thats why Leaflet is global and we can use it!
 // console.log(firstName);
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hiden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hiden');
+});
