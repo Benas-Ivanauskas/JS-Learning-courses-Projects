90bb5808cbc94df0eb60f839ce6b15bca87b63b3
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2024-01-08 08:42:54 +0200

    fixing code

--- Asynchronous JS-Promises,Async-Await, and AJAX/script.js
+++ Asynchronous JS-Promises,Async-Await, and AJAX/script.js
@@ -4,35 +4,29 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
 const getCountryData = function (country) {
-  //old school way doinng AJAX in JS
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
-  // console.log(request.responseText);
-
   request.addEventListener('load', function () {
     console.log(this.responseText);
-    //Converting this here to actual JS object
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    const html = `
+    const httml = `
     <article class="country">
-    <img class="country__img" src="${data.flag}" />
+    <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
     <h3 class="country__name">${data.name.common}</h3>
     <h4 class="country__region">${data.region}</h4>
     <p class="country__row"><span>👫</span>${data.population}</p>
     <p class="country__row"><span>🗣️</span>${data.languages.lit}</p>
-    <p class="country__row"><span>💰</span>${data.currencies}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.EUR.symbol}</p>
     </div>
     </article>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.insertAdjacentHTML('beforeend', httml);
     countriesContainer.style.opacity = 1;
   });
 };
 
 getCountryData('lithuania');
-getCountryData('usa');
+getCountryData('portugal');
